60249ccc665e78bf236a050ec5617e596a293ead
Sean S <sstan97@terpmail.umd.edu>
2017-09-20 10:03:19 -0400

    Milestone 1
    This code DOES NOT work on my machine. Odd issues with function calls being invalid.
    Continuing to explore, but we can use this as a baseline for 408 development.
    To be clear, this is NOT my code and I DO NOT take credit for writing it... the pictures are mine though! :)

---
+++ image_processing/shape_detection.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+MIN_MATCH_COUNT = 10
+
+
+# Open the target image and the scene image
+target =  cv2.imread('closeup.jpg',0)
+scene = cv2.imread('farout.jpg',0)
+
+sift = cv2.xfeatures2d.SIFT_create()
+
+kp1, des1 = sift.detectAndCompute(target,None)
+kp2, des2 = sift.detectAndCompute(scene,None)
+
+
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees=5)
+search_params = dict(checks=50)
+
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+matches = flann.knnMatch(des1,des2,k=2)
+
+good = []
+for m,n in matches:
+    if m.distance < 0.7*n.distance:
+        good.append(m)
+
+if len(good)>MIN_MATCH_COUNT:
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+    matchesMask = mask.ravel().tolist()
+
+    h,w = target.shape
+    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+    dst = cv2.perspectiveTransform(pts,M)
+
+    scene = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+else:
+    print('Not enough good matches. {} found'.format(len(good)))
+    matchesMask = None
+
+
+draw_params = dict(matchColor = (0,255,0),
+        singlePointColor = None,
+        matchesMask = matchesMask,
+        flags = 2)
+
+img3 = cv2.drawMatches(target, kp1, scene, kp2, good, None, **draw_params)
+
+plt.imshow(img3, 'gray'), plt.show()
