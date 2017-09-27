f3ac55138635f63f9c0d23fb567e004c3f00fb03
ztawes <ztawes@gmail.com>
2017-09-27 15:01:46 -0400

    Add files via upload
    Very basic example of sending a char (currently 'L') from Python to arduino. It seems to enter the if(inChar == 'L') statement in the arduino code (or the Python code wouldn't close), but it doesn't always print to the Serial Monitor There is a problem when switching the serial destination (from Python program switching to the Serial Monitor).

---
+++ PythonToArduinoCharTest.py
@@ -0,0 +1,39 @@
+'''
+Created on 2011-12-02
+
+@author: Bobby Wood
+'''
+
+## import the serial library
+import serial
+
+## Boolean variable that will represent 
+## whether or not the arduino is connected
+connected = False
+
+## open the serial port that your ardiono 
+## is connected to.
+## ser = serial.Serial("COM11", 9600)
+ser = serial.Serial("/dev/ttyACM0", 9600)
+
+## loop until the arduino tells us it is ready
+while not connected:
+    serin = ser.read()
+    connected = True
+
+## Tell the arduino to blink!
+## ser.write("L\n".encode())
+
+ser.write('L')
+
+## Wait until the arduino tells us it 
+## is finished blinking
+## while ser.read() == '1':
+##    ser.read()
+	
+while True:
+    if ser.read() == '0':
+		break
+
+## close the port and end the program
+ser.close()
