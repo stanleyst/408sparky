99484a79147c94fc6d397b9a2d9b81602b751fac
Sean S <sstan97@terpmail.umd.edu>
2017-10-03 17:49:30 -0400

    Object Recognition
    I can find the green circle guys :)
    
    Tracking will be done tomorrow in class with a live feed!

--- image_processing/tracking.py
+++ image_processing/tracking.py
@@ -23,5 +23,25 @@ mask = cv2.erode(mask, None, iterations=2)
 mask = cv2.dilate(mask, None, iterations=2)
 
 
-plt.imshow(mask)
+#plt.imshow(mask)
+#plt.show()
+
+cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)[-2]
+center = None
+
+if len(cnts) > 0:
+    c = max(cnts, key=cv2.contourArea)
+    ((x, y), radius) = cv2.minEnclosingCircle(c)
+    M = cv2.moments(c)
+    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+    if radius > 10:
+        cv2.circle(image, (int(x), int(y)), int(radius),
+                (0, 255, 255), 2)
+        cv2.circle(image, center, 5, (0,0,255), -1)
+
+
+
+plt.imshow(image)
 plt.show()
