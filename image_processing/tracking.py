add44d9bf0423cb680d9f26266734062517e27da
Team10 <ztawes@gmail.com>
2017-10-04 16:38:27 -0400

    Just Send It!

--- image_processing/tracking.py
+++ image_processing/tracking.py
@@ -2,60 +2,73 @@ import numpy as np
 import imutils
 import cv2
 from matplotlib import pyplot as plt
+import serial
 
-# Instantiate the camera
-cap = cv2.VideoCapture(0)
+def tracking(ser):
+    # Instantiate the camera
+    cap = cv2.VideoCapture(0)
 
-greenLower = (29,86,6)
-greenUpper = (64, 255, 255)
+    greenLower = (29,86,6)
+    greenUpper = (64, 255, 255)
 
-# I will want to put the following in a while loop later to do
-# tracking live time with a camera feed
+    # I will want to put the following in a while loop later to do
+    # tracking live time with a camera feed
 
-while(True):
+    while(True):
 
-    (ret, image) = cap.read()
+        (ret, image) = cap.read()
 
-    if not ret:
-        break
+        if not ret:
+            break
 
-    image = imutils.resize(image, width=600)
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        image = imutils.resize(image, width=600)
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    # Create a mask for the chosen color
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+        # Create a mask for the chosen color
+        mask = cv2.inRange(hsv, greenLower, greenUpper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
 
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_SIMPLE)[-2]
+        center = None
 
-#plt.imshow(mask)
-#plt.show()
+        if len(cnts) > 0:
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)[-2]
-    center = None
+            if radius > 10:
+                cv2.circle(image, (int(x), int(y)), int(radius),
+                    (0, 255, 255), 2)
+                cv2.circle(image, center, 5, (0,0,255), -1)
 
-    if len(cnts) > 0:
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            lower = 600/4
 
-        if radius > 10:
-            cv2.circle(image, (int(x), int(y)), int(radius),
-                (0, 255, 255), 2)
-            cv2.circle(image, center, 5, (0,0,255), -1)
+            if x <= lower:
+                ser.write('L')
+            elif x >= lower*3:
+                ser.write('R')
+            else:
+                ser.write('S')
+        #cv2.imshow('frame',image)
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+        #    break
 
+    cap.release()
+    cv2.destroyAllWindows()
 
-    cv2.imshow('frame',image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+if __name__ == '__main__':
 
-cap.release()
-cv2.destroyAllWindows()
-    #cv2.imshow('window', image)
+    connected = False
 
-    #plt.imshow(image)
-    #plt.pause(0.1)
-    #plt.show()
-    #plt.close()
+    ser = serial.Serial('/dev/ttyACM0',9600)
+
+    while not connected:
+        serin = ser.read()
+        connected = True
+
+    tracking(ser)
+
+    ser.close()
